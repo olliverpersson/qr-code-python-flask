4f7d3b95c314d1ec4e5ff3d67fa63cb4e8c7e1de
Olliver Persson <olliver.persson@gmail.com>
2019-11-29 03:03:25 -0800

    Added wifi qr code form and created route for /wifi-qr-code

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 class CreateForm(FlaskForm):
     
     textToCreate = StringField('Text for QR Code', validators=[DataRequired()])
+    submit = SubmitField('Create')
+    
+class CreateWifiForm(FlaskForm):
+    
+    wifiName = StringField('WIFI Name', validators=[DataRequired()])
+    wifiType = SelectField('Wifi Type', choices = [('WEP', 'WEP'), ('WPA', 'WPA'), ('nopass', 'No password')], validators=[DataRequired()])
+    wifiPass = PasswordField('Wifi Password')
+    ssidHidden = BooleanField('Use SSID', validators=[DataRequired()])
     submit = SubmitField('Create')
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, url_for, redirect
 from app import app
 import os
 import pyqrcode
-from app.forms import CreateForm
+from app.forms import CreateForm, CreateWifiForm
 from uuid import uuid4 as uuid
 
 basedir = os.path.dirname(__file__)
@@ -29,7 +29,16 @@ def creator():
         
     return render_template('creator.html', title='QR-CODE CREATOR', form=form)
 
+@app.route('/wifi-qr-code', methods=['GET', 'POST'])
+def wifiqr():
+    
+    form = CreateWifiForm()
+    
+    return render_template('wifiqr.html', form=form)
+
+
 @app.route('/qrcode/<string:qrid>', methods=['GET'])
 def created(qrid):
     
     return render_template('created.html', title='QR-CODE CREATED', qrid=qrid)
+
