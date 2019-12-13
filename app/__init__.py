938d5bf996a313303f7cfb245fe16a44b0d28fe9
Olliver Persson <olliver.persson@gmail.com>
2019-12-13 02:48:05 -0800

    Clearing cached qr codes now works

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 import threading
+import pathlib
 
 app = Flask(__name__)
 
@@ -11,22 +12,20 @@ from app import routes
 #Clear qrcodes folder
 def clear_qr_codes():
     
-    print("Clearing cached qrcodes")
+    pth = pathlib.Path('app/static/qrcodes')
     
-    if os.path.exists('static/qrcodes'):
-         os.rmdir('static/qrcodes')
-    
-    os.mkdir('static/qrcodes')
+    for sub in pth.iterdir():
+        if sub.is_dir():
+            delete_folder(sub)
+        else:
+            sub.unlink()
     
     try:
         seconds = int(os.environ['CLEAR_CACHE_SECONDS'])
     except:
         print('CLEAR_CACHE_SECONDS not defined, using 1800 seconds(30 minutes)')
         seconds = 1800
-        
-    print(seconds)
     
     threading.Timer(seconds, clear_qr_codes).start()
-
-print('Running clear qwr codes')    
+  
 clear_qr_codes()
